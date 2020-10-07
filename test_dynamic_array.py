960a9009a034f44fee04731f53721a148c5701f4
Brayden Brown <braydenbrown@gmail.com>
2020-10-07 15:28:59 -0700

    Add exception for index of array 17 tests passed

--- test_dynamic_array.py
+++ test_dynamic_array.py
@@ -218,16 +218,16 @@ class TestDynamicArray(unittest.TestCase):
     # Invalid indexes
     # """
 
-    # def test_negative_index(self):
-    #     """
-    #     Accessing with a negative index raises an IndexError
-    #     """
-    #     a = DynamicArray()
-    #     try:
-    #         a[-1]
-    #         self.fail("Did not raise IndexError: index out of range.")
-    #     except IndexError:
-    #         pass
+    def test_negative_index(self):
+        """
+        Accessing with a negative index raises an IndexError
+        """
+        a = DynamicArray()
+        try:
+            a[-1]
+            self.fail("Did not raise IndexError: index out of range.")
+        except IndexError:
+            pass
 
     # def test_large_index(self):
     #     """
